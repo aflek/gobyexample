b433c4d0e4bede37e92f74c1c2c2be0ace0bb435
Андрей Флек <Andrey.Flek@online-express.ru>
2018-08-21 18:37:12 +0300

    +Variadic Functions

---
+++ Variadic_Functions/variadic_functions.go
@@ -0,0 +1,24 @@
+//Variadic function может быть вызвана с любым числом аргументов
+package main
+
+import (
+	"fmt"
+)
+
+func sum(nums ...int)  {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main()  {
+	
+	sum(1, 2)//[1 2] 3
+	sum(1, 2, 3)//[1 2 3] 6
+
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)//[1 2 4] 10
+}
